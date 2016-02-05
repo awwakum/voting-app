a0be655054fdae57f47350a4f40d269766e44cfd
awwakum <awwakum@gmail.com>
2016-02-05 10:34:55 +0100

    routes

--- server/routes.js
+++ server/routes.js
@@ -4,15 +4,15 @@ var path = require('path');
 
 module.exports = function(app) {
 
-	/*app.get('/', function(req, res) {
+	app.get('/', function(req, res) {
 		res.sendFile(path.join(app.get('appPath') + '/index.html'));
-	});*/
+	});
 
 	app.use('/api/polls', require('./api/poll'));
 	app.use('/api/users', require('./api/user'));
 	app.use('/auth', require('./auth'));
 
-	
+
 	app.route('/*')
 	.get(function(req, res) {
 		res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
